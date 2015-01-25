32788823b3ea6dbd77a10d622b9bcb535e708f53
SheldonLC <pantasiaindie@outlook.com>
2015-01-25 11:18:39 +0800

    INIT_001

---
+++ sievpple/sievpple/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  sievpple
+//
+//  Created by Yin Bo on 15/1/25.
+//  Copyright (c) 2015年 com.PantasiaIndie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
